621aa57444134f72059cbe4e61dbd71353787a34
DanteSan <dante_GH@hotmail.com.ar>
2020-10-18 18:50:19 -0300

    ningun cambio

--- Config/Config.php
+++ Config/Config.php
@@ -1,7 +1,7 @@
 <?php namespace Config;
 
     define('ROOT', str_replace('\\','/',dirname(__DIR__) . "/"));
-
+    
     $base=explode($_SERVER['DOCUMENT_ROOT'],ROOT);
     define("BASE",$base[1]);
 
--- Config/Router.php
+++ Config/Router.php
@@ -1,5 +1,4 @@
 <?php 
-use Config\Request as Request;
 
 namespace Config;
 
