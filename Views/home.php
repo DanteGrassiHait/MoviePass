8dc3bddeeebeb419e0f9eca070c37023ec28e6bf
DanteSan <dante_GH@hotmail.com.ar>
2020-10-28 01:32:06 -0300

    Views user and register

--- Views/home.php
+++ Views/home.php
@@ -3,10 +3,8 @@
 <main class="d-flex align-items-center justify-content-center height-100" >
           <div class="content">
                <div class="container">
-                    <div class="grid"> 
-                         <div class="form_login">
-                              
-                              <div class="form">                 
+                    <div class="grid">                              
+                              <div class="form bg-dark-transparent rounded">                 
                                    <form action="<?php echo FRONT_ROOT ?>User/login" method="POST" class="login-form bg-dark-alpha p-5 bg-light">
                                    <?php
                                         if(isset($error)){
@@ -20,19 +18,18 @@
                                         </div>
                                         <div class="form-group">
                                              <label for="">Password</label>
-                                             <input type="password" name="password" class="form-control form-control-lg" >
+                                             <input type="password" name="password" class="form-control form-control-lg heading nospace" >
                                         </div>
                                         <div class="btn_cont">
-                                        <button class="btn btn-primary btn-block btn-lg" type="submit">Login</button>
+                                        <button class="btn btn-primary btn-block btn-lg heading nospace" type="submit">Login</button>
                                         </div>
                                    </form>
                                    <a href="<?php echo FRONT_ROOT;?>User/ShowRegisterView" class="login-form bg-dark-alpha p-5 bg-light">
                                    <div class="btn_cont">
-                                        <button class="btn btn-primary btn-block btn-lg" >Register</button>
+                                        <button class="btn btn-primary btn-block btn-lg heading nospace" >Register</button>
                                    </div>
                                    </a>
                               </div>
-                         </div>
                     </div>
                </div>
           </div>
--- Views/user/ShowRegisterView.php
+++ Views/user/ShowRegisterView.php
@@ -1,21 +1,30 @@
+<main class="d-flex align-items-center justify-content-center height-100" >
+          <div class="content">
+               <div class="container">
+                    <div class="grid">                              
+                              <div class="form bg-dark-transparent rounded">  
+                                <form action="<?= FRONT_ROOT ?>User/RegisterNew" method="POST">
 
-    <form action="<?= FRONT_ROOT ?>User/RegisterNew" method="POST">
+                                    <label>Nombre</label>
+                                    <input type="text" name="name" class="form-control form-control-lg" required/>
 
-        <label>Nombre</label>
-        <input type="text" name="name" required/>
+                                    <label>Apellido</label>
+                                    <input type="text" name="lastName" class="form-control form-control-lg" required/>
 
-        <label>Apellido</label>
-        <input type="text" name="lastName" required/>
+                                    <label>DNI</label>
+                                    <input type="text" name="id" class="form-control form-control-lg" required/>
 
-        <label>DNI</label>
-        <input type="text" name="id" required/>
+                                    <label>Email</label>
+                                    <input type="email" name="email" class="form-control form-control-lg" required/>
 
-        <label>Email</label>
-        <input type="email" name="email" required/>
+                                    <label>Password</label>
+                                    <input type="password" name="password" class="form-control form-control-lg heading nospace" required/>
 
-        <label>Password</label>
-        <input type="password" name="password" required/>
+                                    <button class="btn btn-primary btn-block btn-lg heading nospace" type="submit">Sign Up</button>
 
-        <button class="btn btn-primary btn-block btn-lg" type="submit">Sign Up</button>
-
-    </form>
+                                </form>
+                            </div>
+                    </div>
+               </div>
+          </div>
+</main>
